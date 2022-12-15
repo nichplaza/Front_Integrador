e856846111f6b3cd3cd26b4ac42724b86eb7bf41
nichplaza <109184041+nichplaza@users.noreply.github.com>
2022-12-15 17:47:01 -0300

    Circulos de Progreso

--- Integrador_NicoPlaza/src/app/app.module.ts
+++ Integrador_NicoPlaza/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { BannerComponent } from './components/banner/banner.component';
 import { AboutComponent } from './components/about/about.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { EducacionComponent } from './components/educacion/educacion.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
