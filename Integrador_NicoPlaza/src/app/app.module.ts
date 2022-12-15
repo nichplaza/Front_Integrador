2e2f6dad10f14e1c083c6794d238a72eb68896f8
nichplaza <109184041+nichplaza@users.noreply.github.com>
2022-12-15 15:36:25 -0300

    Agregado Banner

--- Integrador_NicoPlaza/src/app/app.module.ts
+++ Integrador_NicoPlaza/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgProComponent } from './components/logo-arg-pro/logo-arg-pro.component';
+import { RedessocComponent } from './components/redessoc/redessoc.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgProComponent,
+    RedessocComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
---
+++ Integrador_NicoPlaza/src/app/components/redessoc/redessoc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-redessoc',
+  templateUrl: './redessoc.component.html',
+  styleUrls: ['./redessoc.component.css']
+})
+export class RedessocComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
