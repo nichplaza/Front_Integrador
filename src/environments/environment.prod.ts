4538b1c7b969927bdcb84e1fbe08cbe54cda8bc8
nichplaza <109184041+nichplaza@users.noreply.github.com>
2022-12-29 16:43:19 -0300

    Cambios Finales

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'npfrontend-1d36b',
+    appId: '1:292319148075:web:94b93b85651aa7f3dd526e',
+    storageBucket: 'npfrontend-1d36b.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyDv08jWi23kK_xMgs5wCEKq8S3QVNQJ85A',
+    authDomain: 'npfrontend-1d36b.firebaseapp.com',
+    messagingSenderId: '292319148075',
+  },
   production: true,
   URL: 'https://spotless-nancee-nichplaza.koyeb.app/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'npfrontend-1d36b',
+    appId: '1:292319148075:web:94b93b85651aa7f3dd526e',
+    storageBucket: 'npfrontend-1d36b.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyDv08jWi23kK_xMgs5wCEKq8S3QVNQJ85A',
+    authDomain: 'npfrontend-1d36b.firebaseapp.com',
+    messagingSenderId: '292319148075',
+  },
 
   production: false,
   URL: 'http://localhost:8080/'
